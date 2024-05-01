261429a463ee4b78292a374707e646cda6e2559d
Slinet6056 <git@slinet.me>
2024-05-01 19:27:04 +0800

    Change Date field type in Plan struct

--- internal/model/plan.go
+++ internal/model/plan.go
@@ -1,12 +1,14 @@
 package model
 
+import "time"
+
 // Plan 定义巡检任务的结构体
 type Plan struct {
-	ID          uint   `json:"id" gorm:"primaryKey"`
-	TenantID    uint   `json:"tenant_id"`
-	InspectorID uint   `json:"inspector_id"`
-	Date        string `json:"date"`
-	Status      string `json:"status"`
+	ID          uint      `json:"id" gorm:"primaryKey"`
+	TenantID    uint      `json:"tenant_id"`
+	InspectorID uint      `json:"inspector_id"`
+	Date        time.Time `json:"date"`
+	Status      string    `json:"status"`
 
 	Inspector User `gorm:"foreignKey:InspectorID"`
 }
